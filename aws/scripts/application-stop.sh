306cb757e80b76789be14447261c04877daee813
pamindakw <pamindaya@gmail.com>
2024-07-15 16:38:59 +0530

    removed scripts and workflows

--- aws/scripts/application-stop.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -x
-
-# # System control will return either "active" or "inactive".
-tomcat_running=$(systemctl is-active nginx)
-if [ "$tomcat_running" == "active" ]; then
-    service nginx stop
-fi
